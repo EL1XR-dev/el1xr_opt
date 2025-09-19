228216d073d1a3b3886a1baad913fc2bd958884b
Erik Alvarez <erikfilias@gmail.com>
2025-09-19 17:14:42 +0200

    Update conf

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -107,7 +107,7 @@ html_last_updated_fmt = ''
 html_show_sphinx = False
 html_theme_options = {
     'analytics_id': 'UA-515200-2',  #  Provided by Google in your dashboard
-    'description': 'version 1.0.14',
+    # 'description': 'version 1.0.1',
     'body_max_width' : 'none',
     'page_width': 'auto',
     'font_family': 'Georgia',
