d100d0c667af290d2b43addf04d82d4c819827ea
Erik Alvarez <erikfilias@gmail.com>
2025-09-19 16:59:53 +0200

    adding custom css file

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -112,16 +112,18 @@ html_theme_options = {
     'page_width': 'auto',
     'font_family': 'Georgia',
     'logo_only': True,
+    'display_version': False,
     'collapse_navigation': True,
     'navigation_depth': 1,
     'sticky_navigation': True,
     'style_external_links': True,
 }
-html_js_files = [
-    '../custom.js',
-]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+
+html_js_files = [
+    '../custom.js',
+]
