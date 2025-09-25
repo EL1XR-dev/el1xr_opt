a291c59d5c0d1dc2c705537866a6c562b67662a8
Erik Alvarez <erikfilias@gmail.com>
2025-09-25 23:45:10 +0200

    Update __main__.py

--- src/el1xr_opt/__main__.py
+++ src/el1xr_opt/__main__.py
@@ -1,24 +1,6 @@
 # el1xr_opt/__main__.py
-import argparse
-
-from .oM_Main import main
-from .oM_SolverSetup import ensure_ampl_solvers
-
-for i in range(0, 117):
-    print('-', end="")
-
-print('\nElectricity for Low-carbon Integration and eXchange of Resources (EL1XR) - Version 1.0.1 - Sep 25, 2025')
-print('#### Non-commercial use only ####')
-
-parser = argparse.ArgumentParser(description='Introducing main arguments...')
-parser.add_argument('--dir',    type=str, default=None)
-parser.add_argument('--case',   type=str, default=None)
-parser.add_argument('--solver', type=str, default=None)
-parser.add_argument('--date',   type=str, default=None)
-parser.add_argument('--rawresults', type=str, default=None)
-parser.add_argument('--plots', type=str, default=None)
-
+from .oM_Main import main, ensure_ampl_solvers
 
 if __name__ == "__main__":
     ensure_ampl_solvers()
-    main(parser)
+    main()
