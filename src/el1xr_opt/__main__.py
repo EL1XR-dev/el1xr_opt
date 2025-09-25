9192ff5b99b1d9bb19b801e59c85ff98b0383572
Erik Alvarez <erikfilias@gmail.com>
2025-09-25 23:41:05 +0200

    Update __main__.py

--- src/el1xr_opt/__main__.py
+++ src/el1xr_opt/__main__.py
@@ -1,7 +1,5 @@
 # el1xr_opt/__main__.py
 import argparse
-import datetime
-import os
 
 from .oM_Main import main
 from .oM_SolverSetup import ensure_ampl_solvers
@@ -20,12 +18,6 @@ parser.add_argument('--date',   type=str, default=None)
 parser.add_argument('--rawresults', type=str, default=None)
 parser.add_argument('--plots', type=str, default=None)
 
-default_DirName    = os.path.dirname(__file__)
-default_CaseName   = 'Home1'                              # To select the case
-default_SolverName = 'highs'
-default_date       = datetime.datetime.now().replace(second=0, microsecond=0)
-default_rawresults = 'False'
-default_plots      = 'False'
 
 if __name__ == "__main__":
     ensure_ampl_solvers()
