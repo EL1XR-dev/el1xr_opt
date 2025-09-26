0538c5393b907a0c4d75edfcb165e1b8247a5b9b
Erik Alvarez <erikfilias@gmail.com>
2025-09-26 16:31:45 +0200

    Update __main__.py

--- src/el1xr_opt/__main__.py
+++ src/el1xr_opt/__main__.py
@@ -1,8 +1,5 @@
 # __main__.py
 from .el1xr_Main import main
-from .Modules.oM_SolverSetup import ensure_ampl_solvers
 
 if __name__ == "__main__":
-    # Try to ensure HiGHS AMPL module is installed; do nothing if it already is.
-    ensure_ampl_solvers(["highs"], quiet=True)
     raise SystemExit(main())
