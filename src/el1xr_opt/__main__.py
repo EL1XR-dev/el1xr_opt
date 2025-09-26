5ff3f54f07e5b55d4332e826ed6b74cb70785698
Erik Alvarez <erikfilias@gmail.com>
2025-09-26 15:08:30 +0200

    Update __main__.py

--- src/el1xr_opt/__main__.py
+++ src/el1xr_opt/__main__.py
@@ -1,12 +1,3 @@
-from .el1xr_Main import main
-from .Modules.oM_SolverSetup import ensure_ampl_solvers
-
-if __name__ == "__main__":
-    # Making sure that the solvers are correctly set up
-    ensure_ampl_solvers()
-    raise SystemExit(main())
-
-
 # __main__.py
 from .el1xr_opt import main
 from .Modules.oM_SolverSetup import ensure_ampl_solvers
