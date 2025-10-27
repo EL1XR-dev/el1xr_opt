d405c8a43d03095d21bd93249572044fb7f9272a
Erik Alvarez <erikfilias@gmail.com>
2025-10-27 22:39:05 +0100

    Update oM_Sequence.py

--- src/el1xr_opt/Modules/oM_Sequence.py
+++ src/el1xr_opt/Modules/oM_Sequence.py
@@ -55,9 +55,9 @@ def routine(dir, case, solver, date, rawresults, plots):
     log_time('- Total time for solving the model:', start_time)
     start_time = time.time()
     # outputting the results
-    model = saving_rawdata(dir, case, solver, model, model)
-    log_time('- Total time for outputting the raw data:', start_time)
-    start_time = time.time()
+    # model = saving_rawdata(dir, case, solver, model, model)
+    # log_time('- Total time for outputting the raw data:', start_time)
+    # start_time = time.time()
     # outputting the results
     model = saving_results(dir, case, date, model, model)
     log_time('- Total time for outputting the results:', start_time)
